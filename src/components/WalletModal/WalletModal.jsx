2a684f58be0bebc5dd8fbf7aad014f0a4566d43e
thu <165041915+ThuPham60232004@users.noreply.github.com>
2024-10-02 21:28:39 +0700

    THU

--- src/components/WalletModal/WalletModal.jsx
+++ src/components/WalletModal/WalletModal.jsx
@@ -1,20 +1,28 @@
-import React,{useState} from "react";
-import "./WalletModal.css";
+import React, { useState } from "react";
 import Web3Modal from "web3modal";
 import { Web3Provider } from '@ethersproject/providers';
+import "./WalletModal.css";
+
 const WalletModal = ({ onClose }) => {
     const [provider, setProvider] = useState(null);
 
     const connectWallet = async () => {
         try {
-            const web3Modal = new Web3Modal();
+            const web3Modal = new Web3Modal({
+                cacheProvider: false,
+                providerOptions: {}
+            });
             const instance = await web3Modal.connect();
             const provider = new Web3Provider(instance);
             setProvider(provider);
+
+            const accounts = await provider.listAccounts();
+            console.log("Connected account:", accounts[0]);
         } catch (error) {
             console.error("Failed to connect wallet", error);
         }
     };
+
     return (
         <div className="modal-overlay_modal" onClick={onClose}>
             <div className="wallet-login_modal" onClick={(e) => e.stopPropagation()}>
@@ -24,20 +32,20 @@ const WalletModal = ({ onClose }) => {
                 </div>
                 
                 <div className="login-options_modal">
-                    <div className="wallet-option_modal" >
-                    <img src='https://forum.zeroqode.com/uploads/default/original/2X/4/401498d7adfbb383fea695394f4f653ea4e7c9a7.png' alt="MetaMask" className="icon_modal" />
+                    <div className="wallet-option_modal" onClick={connectWallet}>
+                        <img src='https://forum.zeroqode.com/uploads/default/original/2X/4/401498d7adfbb383fea695394f4f653ea4e7c9a7.png' alt="MetaMask" className="icon_modal" />
                     </div>
-                    <div className="wallet-option_modal">
+                    <div className="wallet-option_modal" onClick={connectWallet}>
                         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5AsJoB6E5HdBs6ClI0rKXYSkfK_imGv5LbA&s' alt="TrustWallet" className="icon_modal" />
                     </div>
-                    <div className="wallet-option_modal">
-                        <img src='https://chainbroker.io/_next/image/?url=https%3A%2F%2Fstatic.chainbroker.io%2Fmediafiles%2Fprojects%2Fwalletconnect%2Fwalletconnect.jpeg&w=2560&q=75' alt="MetaMask" className="icon_modal" />
+                    <div className="wallet-option_modal" onClick={connectWallet}>
+                        <img src='https://chainbroker.io/_next/image/?url=https%3A%2F%2Fstatic.chainbroker.io%2Fmediafiles%2Fprojects%2Fwalletconnect%2Fwalletconnect.jpeg&w=2560&q=75' alt="WalletConnect" className="icon_modal" />
                     </div>
-                    <div className="wallet-option_modal">
+                    <div className="wallet-option_modal" onClick={connectWallet}>
                         <img src='https://static-00.iconduck.com/assets.00/coinbase-icon-2048x2048-oq45l9cy.png' alt="Coinbase" className="icon_modal" />
                     </div>
-                    <div className="wallet-option_modal">
-                        <img src='https://spiritswap.finance/_next/static/media/paraSwap.b94e1099.png' alt="Coinbase" className="icon_modal" />
+                    <div className="wallet-option_modal" onClick={connectWallet}>
+                        <img src='https://spiritswap.finance/_next/static/media/paraSwap.b94e1099.png' alt="ParaSwap" className="icon_modal" />
                     </div>
                 </div>
 
