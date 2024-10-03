dd7a808644e40d7bde6312be64fec5a0595d7440
thu <165041915+ThuPham60232004@users.noreply.github.com>
2024-10-03 19:04:44 +0700

    THU

--- src/pages/CollectionDetailPage/CollectionDetailPage.jsx
+++ src/pages/CollectionDetailPage/CollectionDetailPage.jsx
@@ -1,48 +1,53 @@
-import React,{useContext}from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { NavLink, Outlet, useNavigate } from 'react-router-dom';
 import './CollectionDetailPage.css';
 import HeaderDetailCollection from '../../components/CollectionDetailComponents/HeaderDetailCollection/HeaderDetailCollection';
 import Navbar from '../../components/navbar/Navbar';
 import { ItemContext } from '../../context/ItemContext';
+
 const CollectionDetailPage = () => {
     const { item } = useContext(ItemContext);
-  return (
-    <div className='CollectionDetailPageContainer'>
-      <Navbar />
-      <br/> <br/> <br/> <br/> <br/>
-      <HeaderDetailCollection item={item} />
-      <div className='CollectionDetailPageSelect'>
-        <div className='CollectionDetailPageItems'>
-          <NavLink to="overview" activeClassName="active">
-            <h3>Overview</h3>
-          </NavLink>
-        </div>
-        <div className='CollectionDetailPageItems'>
-          <NavLink to="items" activeClassName="active">
-            <h3>Items</h3>
-          </NavLink>
-        </div>
-        <div className='CollectionDetailPageItems'>
-          <NavLink to="offers" activeClassName="active">
-            <h3>Offers</h3>
-          </NavLink>
-        </div>
-        <div className='CollectionDetailPageItems'>
-          <NavLink to="analytics" activeClassName="active">
-            <h3>Analytics</h3>
-          </NavLink>
-        </div>
-        <div className='CollectionDetailPageItems'>
-          <NavLink to="activity" activeClassName="active">
-            <h3>Activity</h3>
-          </NavLink>
-        </div>
-      </div>
+    const navigate = useNavigate();
 
-    
-      <Outlet />
-    </div>
-  );
+    useEffect(() => {
+        navigate("overview"); 
+    }, [navigate]);
+
+    return (
+        <div className='CollectionDetailPageContainer'>
+            <Navbar />
+            <br/> <br/> <br/> <br/> <br/>
+            <HeaderDetailCollection item={item} />
+            <div className='CollectionDetailPageSelect'>
+                <div className='CollectionDetailPageItems'>
+                    <NavLink to="overview" activeClassName="active">
+                        <h3>Overview</h3>
+                    </NavLink>
+                </div>
+                <div className='CollectionDetailPageItems'>
+                    <NavLink to="items" activeClassName="active">
+                        <h3>Items</h3>
+                    </NavLink>
+                </div>
+                <div className='CollectionDetailPageItems'>
+                    <NavLink to="offers" activeClassName="active">
+                        <h3>Offers</h3>
+                    </NavLink>
+                </div>
+                <div className='CollectionDetailPageItems'>
+                    <NavLink to="analytics" activeClassName="active">
+                        <h3>Analytics</h3>
+                    </NavLink>
+                </div>
+                <div className='CollectionDetailPageItems'>
+                    <NavLink to="activity" activeClassName="active">
+                        <h3>Activity</h3>
+                    </NavLink>
+                </div>
+            </div>
+            <Outlet />
+        </div>
+    );
 }
 
 export default CollectionDetailPage;
