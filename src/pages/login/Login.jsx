78fba8f72630c011ff24a881ac1488b2017c32fd
camtu470 <truongtu09821@gmail.com>
2024-10-04 15:10:42 +0700

    tu

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,9 @@
-
 import React, { useState } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
-import   
- { FaUserAlt, FaLock } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
+import { FaFacebookF,FaGoogle } from "react-icons/fa";
+import { FaArrowRightLong } from "react-icons/fa6";
 import './login.css';
 
 const Login = () => {
@@ -39,49 +38,68 @@ const Login = () => {
   };
 
   return (
-    <div className="background-container_login">
-      <img src='/img/background.jpg' alt="" />
-      <div className="wrapper_login">
-        <div className="form-box_login">
-          <form onSubmit={handleSubmit} className='form_login'>
-            <h1>Login</h1>
-            <div className="input-box_login">
-              <input
-                type="text"
-                placeholder="Người dùng"
+    <div className="login_page">
+      <div className="login_container">
+        <div className="left_container_login" onSubmit={handleSubmit}>
+          <div className="logo_login_con">
+              <img src="./img/collection.png" alt="" className='logo_login'/>
+             <h2 className='title_login'>LOGIN</h2>
+          </div>
+          <div className="body_login_container_left">
+            <div className="form_field" >
+              <div className="input_group_login">
+                <input type="text" className="input_info_login"  
                 id='username'
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
-                required
-              />
-              <FaUserAlt className="icon_login_page" />
-            </div>
-            <div className="input-box_login">
-              <input
-                type="password"
-                placeholder="Mật khẩu"
+                required />
+                <label className="name_label" htmlFor="email">Email</label>
+              </div>
+                <div className="input_group_login">
+                <input type="password" className="input_info_login" 
                 id='password'
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <FaLock className="icon_login_page" />
+                required />
+                <label className="name_label" htmlFor="password">Password</label>
+              </div>
             </div>
-            <div className="remember-forgot_login">
+           <div className="action_login">
+            <div className="checkbox_group_login">
+              <input type="checkbox"/>
+              <label className='rememberme' htmlFor="rememberMe">Remember me</label>
+            </div>
+            <a href="/forgotpassword">Forgot password?</a>
+          </div>
 
-              <a href="/forgotpassword">Quên mật khẩu?</a>
+         
+              <button>Login</button>
+          
+          
+            <div className="social_login">
+              
+              <p>Or use your account</p>
+              <div className="social_icons_login">
+                <a href="#"><FaFacebookF className='icon_social_login'/></a>
+                <a href="#"><FaGoogle className='icon_social_login'/></a>
+              </div>
             </div>
-            <button className="submit_login">Đăng nhập</button>
-            <div className="register-link">
-              <p>
-                Bạn chưa có tài khoản? <a href="#" onClick={registerLink}>Đăng ký</a>
-              </p>
+            <div className="text_register">
+              <p>No account yet ? <span><a href="#" onClick={registerLink}>Register here</a></span></p>
             </div>
-          </form>
-          {error && <p className="error-message">{error}</p>}
+          </div>
+        </div>
+         {error && <p className="error-message">{error}</p>}
+        <div className="right_container_login">
+        
+        <h1>Welcome to the FINTECH</h1>
+        <p>If you don't have an account yet, join us and discover a new world of digital ownership.</p>
+        <div className="btn_register">
+              <button onClick={registerLink}>Resister<FaArrowRightLong className='icon_arrow_register'/></button>
+            </div>
         </div>
       </div>
-      <ToastContainer />
+       <ToastContainer />
     </div>
   );
 };
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,10 +1,147 @@
 
+// import React, { useState } from 'react';
+// import 'react-toastify/dist/ReactToastify.css';
+// import { ToastContainer, toast } from 'react-toastify';
+// import { FaUserAlt, FaLock, FaPhoneAlt } from 'react-icons/fa';
+// import { MdOutlinePassword } from "react-icons/md";
+// import { useNavigate } from 'react-router-dom';
+// import './register.css';
+
+// const Register = () => {
+//   const navigate = useNavigate();
+//   const [emailOrPhone, setEmailOrPhone] = useState('');
+//   const [lastName, setLastName] = useState('');
+//   const [middleAndFirstName, setMiddleAndFirstName] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [confirmPassword, setConfirmPassword] = useState('');
+//   const [referralCode, setReferralCode] = useState('');
+//   const [error, setError] = useState(null);
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     // Validate phone number
+//     if (emailOrPhone.length !== 10 || isNaN(emailOrPhone)) {
+//       toast.error('Số điện thoại phải đủ 10 ký tự và là số.');
+  
+//       return;
+//     }
+
+//     // Validate password match
+//     if (password !== confirmPassword) {
+//        toast.error('Mật khẩu và xác nhận mật khẩu không trùng khớp.');
+   
+//       return;
+//     }
+ 
+//     try {
+   
+//       setError(null);
+//       toast.success('Registration successful!');
+//       navigate('/login'); 
+//     } catch (error) {
+//       setError(error.message);
+//       toast.error('Registration failed. Please check your input.');
+//     }
+//   };
+
+//   const loginLink = () => {
+//     navigate('/login');
+//   };
+
+//   return (
+//     <div className="background-container_register">
+//       <img src='/img/background.jpg' alt="" />
+//       <div className="wrapper_register">
+//         <div className="form-box_register">
+//           <form onSubmit={handleSubmit}>
+//             <h1>Đăng kí</h1>
+//             <div className="input-box_register">
+//               <input
+//                 type="text"
+//                 id="emailOrPhone"
+//                 placeholder="Email/Số điện thoại"
+//                 value={emailOrPhone}
+//                 onChange={(e) => setEmailOrPhone(e.target.value)}
+//                 required
+//               />
+//               <FaPhoneAlt className="icon_register_page" />
+//             </div>
+//             <div className="input-box_register">
+//               <input
+//                 type="text"
+//                 id="lastName"
+//                 placeholder="Họ"
+//                 value={lastName}
+//                 onChange={(e) => setLastName(e.target.value)}
+//                 required
+//               />
+//               <FaUserAlt className="icon_register_page" />
+//             </div>
+//             <div className="input-box_register">
+//               <input
+//                 type="text"
+//                 id="middleAndFirstName"
+//                 placeholder="Tên đệm và tên"
+//                 value={middleAndFirstName}
+//                 onChange={(e) => setMiddleAndFirstName(e.target.value)}
+//                 required
+//               />
+//               <FaUserAlt className="icon_register_page" />
+//             </div>
+//             <div className="input-box_register">
+//               <input
+//                 type="password"
+//                 id="password"
+//                 placeholder="Mật khẩu"
+//                 value={password}
+//                 onChange={(e) => setPassword(e.target.value)}
+//                 required
+//               />
+//               <FaLock className="icon_register_page" />
+//             </div>
+//             <div className="input-box_register">
+//               <input
+//                 type="password"
+//                 id="confirmPassword"
+//                 placeholder="Nhập lại mật khẩu"
+//                 value={confirmPassword}
+//                 onChange={(e) => setConfirmPassword(e.target.value)}   
+
+//                 required
+//               />
+//               <MdOutlinePassword className="icon_register_page" />
+//             </div>
+    
+//             <button className="submit_register">Đăng ký</button>
+//             <div className="register-link">
+//               <p>
+//                 Bạn đã có tài khoản? <a href="#" onClick={loginLink}>Đăng nhập</a>
+//               </p>
+//             </div>
+//           </form>
+//           {error && <p className="error-message">{error}</p>}
+//         </div>
+//       </div>
+//       <ToastContainer />
+//     </div>
+//   );
+// };
+
+// export default Register;
+
+
+
+
+
 import React, { useState } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
 import { FaUserAlt, FaLock, FaPhoneAlt } from 'react-icons/fa';
 import { MdOutlinePassword } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
+import { FaArrowRightLong } from "react-icons/fa6";
+import { FaFacebookF,FaGoogle } from "react-icons/fa";
 import './register.css';
 
 const Register = () => {
@@ -50,82 +187,66 @@ const Register = () => {
   };
 
   return (
-    <div className="background-container_register">
-      <img src='/img/background.jpg' alt="" />
-      <div className="wrapper_register">
-        <div className="form-box_register">
-          <form onSubmit={handleSubmit}>
-            <h1>Đăng kí</h1>
-            <div className="input-box_register">
-              <input
-                type="text"
-                id="emailOrPhone"
-                placeholder="Email/Số điện thoại"
-                value={emailOrPhone}
-                onChange={(e) => setEmailOrPhone(e.target.value)}
-                required
-              />
-              <FaPhoneAlt className="icon_register_page" />
+   <div className="register_page">
+      <div className="register_container">
+        <div className="left_container_register" onSubmit={handleSubmit}>
+          <div className="logo_register_con">
+              <img src="./img/collection.png" alt="" className='logo_register'/>
+             <h2 className='title_register'>Register</h2>
+          </div>
+          <div className="body_register_container_left">
+            <div className="form_field" >
+              <div className="input_group_register">
+                <input type="text" className="input_info_register"  required />
+                <label className="name_label" htmlFor="email">Email</label>
+              </div>
+              <div className="input_group_register">
+                <input type="text" className="input_info_register"required />
+                <label className="name_label">Last name</label>
+              </div>
+              <div className="input_group_register">
+                <input type="text" className="input_info_register"required />
+                <label className="name_label">Middle and first name</label>
+              </div>
+              <div className="input_group_register">
+                <input type="password" className="input_info_register"required />
+                <label className="name_label">Password</label>
+              </div>
+              <div className="input_group_register">
+                <input type="Password" className="input_info_register"required />
+                <label className="name_label">Confirm password</label>
+              </div>
             </div>
-            <div className="input-box_register">
-              <input
-                type="text"
-                id="lastName"
-                placeholder="Họ"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-                required
-              />
-              <FaUserAlt className="icon_register_page" />
+         
+              <button>Register</button>
+          
+            <div className="social_register">
+              
+              <p>Or use your account</p>
+              <div className="social_icons_register">
+                <a href="#"><FaFacebookF className='icon_social_register'/></a>
+                <a href="#"><FaGoogle className='icon_social_register'/></a>
+              </div>
             </div>
-            <div className="input-box_register">
-              <input
-                type="text"
-                id="middleAndFirstName"
-                placeholder="Tên đệm và tên"
-                value={middleAndFirstName}
-                onChange={(e) => setMiddleAndFirstName(e.target.value)}
-                required
-              />
-              <FaUserAlt className="icon_register_page" />
+            <div className="text_register">
+              <p>Already have an account ? <span><a href="#">Login here</a></span></p>
             </div>
-            <div className="input-box_register">
-              <input
-                type="password"
-                id="password"
-                placeholder="Mật khẩu"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <FaLock className="icon_register_page" />
+          </div>
+        </div>
+         {error && <p className="error-message">{error}</p>}
+        <div className="right_container_register">
+        
+        <h1>Welcome to the FINTECH</h1>
+        <p>If you have an account yet, login and discover a new world of digital ownership.</p>
+        <div className="btn_register">
+              <button>Login<FaArrowRightLong className='icon_arrow_register'/></button>
             </div>
-            <div className="input-box_register">
-              <input
-                type="password"
-                id="confirmPassword"
-                placeholder="Nhập lại mật khẩu"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}   
-
-                required
-              />
-              <MdOutlinePassword className="icon_register_page" />
-            </div>
-    
-            <button className="submit_register">Đăng ký</button>
-            <div className="register-link">
-              <p>
-                Bạn đã có tài khoản? <a href="#" onClick={loginLink}>Đăng nhập</a>
-              </p>
-            </div>
-          </form>
-          {error && <p className="error-message">{error}</p>}
         </div>
       </div>
-      <ToastContainer />
+       <ToastContainer />
     </div>
   );
 };
 
 export default Register;
+
