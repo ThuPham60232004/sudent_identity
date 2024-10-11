e9c4b4073c870ac2c775ec2968ec67b0f35b84a2
camtu470 <truongtu09821@gmail.com>
2024-10-11 11:33:11 +0700

    tu

--- src/components/CollectionDetailComponents/OverviewDetailCollection/OverviewDetailCollection.jsx
+++ src/components/CollectionDetailComponents/OverviewDetailCollection/OverviewDetailCollection.jsx
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 import './OverviewDetailCollection.css'
 import { FaChevronDown } from "react-icons/fa";
 import { SiTicktick } from "react-icons/si";
+import { TiTick } from "react-icons/ti";
 const OverviewDetailCollection = () => {
   const [activeIndex, setActiveIndex] = useState(null);
 
@@ -26,90 +27,86 @@ const OverviewDetailCollection = () => {
     "When is the reveal?"
   ];
   return (
-    <div className='OverviewDetailCollection'>
-      <div className='OverviewDetailCollectionContainer'>
-      <img
-        src='/img/nft-image-1.png'
-        alt='collectionimage1'
-        className='collection-image-1'
-      />
-      <div className='OverviewDetailCollectionRight'>
-        <div className='OverviewDetailCollectionHeader'>
-          <h5>Items minted</h5>
-          <h6>11.001</h6>
-        </div>  
-        <div className='OverviewDetailCollectionItem'></div>
-        <div className='OverviewDetailCollectionBody'>
-          <h3>Mint sold out</h3>
-          <h5>All items were minted</h5>
-          <div className='OverviewDetailCollectionBodybutton'>
-            <h4>View collection</h4>
+
+    <div className="OverviewDetailCollection">
+        <div className="OverviewDetailCollectionContainer1">
+          <div className="left_OverviewDetailCollectionContainer1">
+            <img src='/img/nft-image-1.png' alt='collectionimage1'className='collection-image-1'/>
           </div>
-        </div>
-        <h3>Mint schedule</h3>
-        <div className='OverviewDetailCollectionFotter'>
-          <SiTicktick />
-          <div className='OverviewDetailCollectionFotterContainer'>
-            <div className='OverviewDetailCollectionFotteritem1'>
-              <h4>Public Stage</h4>
-              <h6>July 12, 2024 at 4:00AM GMT+7</h6>
+          <div className="right_OverviewDetailCollectionContainer1">
+            <div className="mind_ended_con_overview">
+                <h3>Mint ended</h3>
+                <p>81,188 items were minted</p>
+                <button className='view_collection'>View Collection</button>
             </div>
-            <div className='OverviewDetailCollectionFotteritem2'>
-              <h6>Ended</h6>
-              <FaChevronDown />
+            <div className="content_mind_ended_con_overview">
+              <h3>Mind schedule</h3>
+              <div className="detail_content_mind_con">
+                <div className="detail_content_mind_tick">
+                  <TiTick/>
+                </div>
+                <div className="detail_content_mind">
+                  <h2>Public stage</h2>
+                  <h3>Starts: October 8 at 11:00 PM GMT+7</h3>
+                  <h3>Ends: October 9 at 11:00 PM GMT+7</h3>
+                  <p>0.0004 ETH</p>
+                  <p>Limit: 5 per wallet</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        </div>
-        </div>
-        <div className='OverviewDetailCollectionContent1Container'>
-            <div className='OverviewDetailCollectionContent1Left'>
-              <h1>Pararell Avatars: Behind The Scnes</h1>
-              <h2>
-              NFTs, or Non-Fungible Tokens, are unique digital assets that are stored and verified on a blockchain. Unlike cryptocurrencies such as Bitcoin, which are interchangeable, NFTs are non-fungible, meaning each one is unique and cannot be exchanged on a one-to-one basis. They represent ownership of a specific item, which could range from digital art and music to videos or virtual real estate.
-              </h2>
+        <div className="OverviewDetailCollectionContainer3">
+            <div className="left_OverviewDetailCollectionContainer3">
+               <img src='/img/nft-image-2.png' alt='collectionimage1'className='collection-image-1'/>
             </div>
-            <div className='OverviewDetailCollectionContent1Right'>
-              <img
-                  src='/img/nft-image-2.png'
-                  alt='collectionimage1'
-                  className='collection-image-1'
-                />
+            <div className="right_OverviewDetailCollectionContainer3">
+              <div className="content_right_overview_con3">
+                 <h3>Built by Ready Player Me</h3>
+                <p>Ready Player Me is an avatar tech leader used by 4,000 developers globally, delivering 10M+ avatars to games and apps every month. We offer avatar SDKs and APIs that developers can integrate into their apps and games to build avatar systems and save time, boost engagement and unlock new revenue streams.</p>
+                <p>Backed by a16z and a roster of tech, web3, and gaming leaders (including the co-founders of King, Twitch, Github, and gmoney), our mission is to build bridges between virtual worlds to create an open and unified digital experience for users.</p>
+              </div>
+               
+            </div>
+        </div>
+        <div className="OverviewDetailCollectionContainer4">
+          <div className="left_OverviewDetailCollectionContainer4">
+            <div className="left_overview_con4">
+                <h3>Here's what you can do</h3>
+              <ul>
+                <li>Customize and level up your avatar with digital collectibles</li>
+                <li>Immediately use your avatar in games and virtual worlds</li>
+                <li>Own an identity that evolves with you across social platforms</li>
+                <li>Create your avatar today: <span><a href="#">playerzero.me</a></span></li>
+              </ul>
+            </div>
+            </div>
+            <div className="right_OverviewDetailCollectionContainer4">
+               <img src='/img/nft-image-3.png' alt='collectionimage1'className='collection-image-1'/>
+            </div>
+        </div>
+        <div className="OverviewDetailCollectionContainer5">
+            <div className="left_OverviewDetailCollectionContainer5">
+                <h1>FAQ</h1>
+            </div>
+            <div className="right_OverviewDetailCollectionContainer5">
+                <div className="faq-accordion">
+                  {faqItems.map((item, index) => (
+                    <div className="faq-item" key={index}>
+                      <div className="faq-question" onClick={() => toggleAccordion(index)}>
+                        {item}
+                        <span className={`arrow ${activeIndex === index ? 'open' : ''}`}>&#x25BC;</span>
+                      </div>
+                      <div className={`faq-answer ${activeIndex === index ? 'active' : ''}`}>
+                        <p>This is the answer to the question "{item}".</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
             </div>
         </div>
 
-        <div className='OverviewDetailCollectionContent2Container'>
-            <div className='OverviewDetailCollectionContent2Left'>
-            <h1>Kathari</h1>
-              <h2>
-              Using blockchain technology, NFTs ensure that the authenticity and ownership of digital assets are transparent and verifiable. Many artists and creators have embraced NFTs as a new way to monetize their work, offering collectors exclusive ownership of rare digital items. The Ethereum blockchain is currently one of the most popular platforms for minting and trading NFTs, making it a hub for NFT marketplaces.
-              </h2>
-            </div>
-            <div className='OverviewDetailCollectionContent2Right'>
-              <img
-                  src='/img/nft-image-3.png'
-                  alt='collectionimage1'
-                  className='collection-image-1'
-                />
-            </div>
-        </div>
-        <div className="faq-accordion">
-          {faqItems.map((item, index) => (
-            <div className="faq-item" key={index}>
-              <div className="faq-question" onClick={() => toggleAccordion(index)}>
-                {item}
-                <span className={`arrow ${activeIndex === index ? 'open' : ''}`}>&#x25BC;</span>
-              </div>
-              <div className={`faq-answer ${activeIndex === index ? 'active' : ''}`}>
-                <p>This is the answer to the question "{item}".</p>
-              </div>
-            </div>
-          ))}
-      </div>
-
-    
     </div>
-    
   )
 }
 
