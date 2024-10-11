77c9e85305c6147369df60cd697926f7d48f50ff
camtu470 <truongtu09821@gmail.com>
2024-10-11 07:43:21 +0700

    tu

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Services.css'; 
+import React,{useState} from "react";
+
+import { GrFormPrevious,GrFormNext } from "react-icons/gr";
+
 
 const Services = () => {
   const services = [
@@ -20,9 +23,22 @@ const Services = () => {
       id: 3,
       title: 'Advanced Service',
       description: 'Describe important details like price, value, length of service, and why it’s unique. Or use these sections to showcase different key values of your products or services.',
+      image: 'https://th.bing.com/th/id/OIP.n8httTbaeM5ewQvOh9OeZAHaEK?rs=1&pid=ImgDetMain',
+    },
+     {
+      id: 4,
+      title: 'Advanced Service',
+      description: 'Describe important details like price, value, length of service, and why it’s unique. Or use these sections to showcase different key values of your products or services.',
       image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGU03ODTEKiXVfLwVztN4zONd1tOXxebESzA&s',
     },
+     {
+      id: 5,
+      title: 'Advanced Service',
+      description: 'Describe important details like price, value, length of service, and why it’s unique. Or use these sections to showcase different key values of your products or services.',
+      image: 'https://cdn.trendhunterstatic.com/thumbs/483/luxy.jpeg?auto=webp',
+    },
   ];
+
 
   const navigate = useNavigate();
 
@@ -30,16 +46,44 @@ const Services = () => {
     navigate(`/service/${id}`);
   };
 
+ const [currentIndex, setCurrentIndex] = useState(0);
+ const itemsPerPage = 3;
+ const handleNext = () => {
+    if (currentIndex < services.length - itemsPerPage) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+
   return (
     <div className="services-container">
-      <h1>Our Services</h1>
-      <div className="services-grid">
-        {services.map(service => (
+      <div className="title_services">
+        <h1>Our Services</h1>
+        <div className="pre_next_services_con">
+            <GrFormPrevious onClick={handlePrev} />
+            <GrFormNext onClick={handleNext} />
+        </div>
+      </div>
+       <div className="services-grid">
+      {services.slice(currentIndex, currentIndex + itemsPerPage).map(service => (
           <div className="service-card" key={service.id} onClick={() => handleClick(service.id)}>
             <img src={service.image} alt={service.title} className="service-image" />
             <div className="service-details">
-              <h2>{service.title}</h2>
-              <p>{service.description}</p>
+              <h2>{service.id}.{service.title}</h2>
+               {service.description.length > 100 ? (
+                <p>
+                  {service.description.slice(0, 100)}
+                  <span className="truncated-text">...</span>
+                </p>
+              ) : (
+                <p>{service.description}</p>
+              )}
               <h3>READ MORE</h3>
             </div>
           </div>
