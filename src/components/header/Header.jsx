59f932e51dc35136a8186be1753bccb244b492fa
thu <165041915+ThuPham60232004@users.noreply.github.com>
2024-10-15 14:07:25 +0700

    THU

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import "./header.css";
-
+import { Link } from "react-router-dom";
 const Header = () => {
   return (
 <div class="header">
@@ -16,7 +16,12 @@ const Header = () => {
       <div className="content3_header">
           <p>Create a <span className="bold-text">Full-Functional NFT Marketplace,</span> customize the storefront, and list unlimited NFTs. </p>
       </div>
-
+      <Link to='/CreatePage' style={{textDecoration:"none", color:"white"}}>
+      <div className='header_button'>
+       
+       <h3>Create Collection</h3>
+     </div></Link>
+     
     </div>
 
     <div className="right_content_header">
